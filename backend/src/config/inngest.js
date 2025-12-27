1c3fce928a3b29bed29b7ccf9372a15b5565abac
vceeeze <vceeeze11@gmail.com>
2025-12-28 00:26:09 +0100

    typo correction

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -13,7 +13,7 @@ const syncUser = inngest.createFunction(
 
         const newUser = {
             clerkId: id,
-            email: email_addresses[0]?.email_addresses,
+            email: email_addresses[0]?.email_address,
             name: `${first_name || ""} ${last_name || ""} || "User"`,
             imageUrl: image_url,
             addresses: [],
