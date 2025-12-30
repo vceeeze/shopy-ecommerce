7e576c95f0aeb93a7647713db29d7d783745cd51
vceeeze <vceeeze11@gmail.com>
2025-12-30 13:46:11 +0100

    tiny auth.middleware fix

--- backend/src/routes/review.route.js
+++ backend/src/routes/review.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { protectRoute } from "../middleware/auth.middleware";
+import { protectRoute } from "../middleware/auth.middleware.js";
 import { createReview, deleteReview } from "../controllers/review.controller.js";
 
 const router = Router()
