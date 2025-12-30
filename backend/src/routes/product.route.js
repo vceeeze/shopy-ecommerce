d3729f163874451aabca54d0277471bec71f0f12
vceeeze <vceeeze11@gmail.com>
2025-12-30 13:51:24 +0100

    import err fix

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { protectRoute } from "../middleware/auth.middleware";
-import { getAllProducts } from "../controllers/admin.controller";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllProducts } from "../controllers/admin.controller.js";
+import { getProductById } from "../controllers/product.controller.js";
 
 const router = Router()
 
